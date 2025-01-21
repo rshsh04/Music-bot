e0631ec2cd4b1392acadb3824326aa6c740b751a
rshsh04 <rsheed.tv4@gmail.com>
2025-01-21 19:56:51 +0100

    a

--- commands/music/rap.js
+++ commands/music/rap.js
@@ -5,7 +5,7 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const { Translate } = require('../../process_tools');
 
 module.exports = {
-  name: 'tarab',
+  name: 'rap',
   description: 'Play pre-saved YouTube playlists',
   voiceChannel: true,
 
